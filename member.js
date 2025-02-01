1ff6491f3dd5f8a612e414024cb8648fd748914c
Saurabh Ji <106741205+gaur80@users.noreply.github.com>
2025-02-01 10:50:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  const member = {
+    skills: ["JavaScript", "React", "Node", "CSS"],
+    details: {
+      name: "John",
+      age: 22,
+    },
+  };
+
+  return member;
+}
